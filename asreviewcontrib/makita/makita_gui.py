5664bf86f9946100601bc91be5d3c195388310c7
Jelle Teijema <j.j.teijema@gmail.com>
2024-12-19 01:52:49 +0100

    Merge files

--- asreviewcontrib/makita/makita_gui.py
+++ asreviewcontrib/makita/makita_gui.py
@@ -1,9 +1,148 @@
 from pathlib import Path
+from typing import Literal
 
 import streamlit as st
 from asreview import config as ASREVIEW_CONFIG
+from streamlit.components.v1 import html
 
-from asreviewcontrib.makita.st_fixed_container import st_fixed_container
+# https://gist.github.com/toolittlecakes/cf1a5d734cbf5b0b2581c28b2530fec2
+
+
+def st_fixed_container(
+    *,
+    height: int | None = None,
+    border: bool | None = None,
+    mode: Literal["fixed", "sticky"] = "fixed",
+    position: Literal["top", "bottom"] = "top",
+    margin: str | None = None,
+    key: str | None = None,
+):
+    if margin is None:
+        margin = {"top": "2.875rem", "bottom": "0"}[position]
+
+    fixed_container_css = """
+
+div[data-testid="stVerticalBlockBorderWrapper"]:has(div.fixed-container-{id}):not(:has(div.not-fixed-container)){{
+    background-color: transparent;
+    position: {mode};
+    width: inherit;
+    background-color: inherit;
+    {position}: {margin};
+    z-index: 999;
+
+}}
+
+div[data-testid="stVerticalBlockBorderWrapper"]:has(div.fixed-container-{id}):not(:has(div.not-fixed-container)) div[data-testid="stVerticalBlock"]:has(div.fixed-container-{id}):not(:has(div.not-fixed-container)) > div[data-testid="element-container"] {{
+    display: none;
+}}
+
+
+div[data-testid="stVerticalBlockBorderWrapper"]:has(div.not-fixed-container):not(:has(div[class^='fixed-container-'])) {{
+    display: none;
+}}
+""".strip()
+
+    fixed_container = st.container()
+    non_fixed_container = st.container()
+    css = fixed_container_css.format(
+        mode=mode,
+        position=position,
+        margin=margin,
+        id=key,
+    )
+    with fixed_container:
+        st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
+        st.markdown(
+            f"<div class='fixed-container-{key}'></div>",
+            unsafe_allow_html=True,
+        )
+    with non_fixed_container:
+        st.markdown(
+            "<div class='not-fixed-container'></div>",
+            unsafe_allow_html=True,
+        )
+
+    opaque_container_js = """
+const root = parent.document.querySelector('.stApp');
+let lastBackgroundColor = null;
+
+
+function updateContainerBackground(currentBackground) {
+    parent.document.documentElement.style.setProperty('--background-color', currentBackground);
+    ;
+}
+
+function checkForBackgroundColorChange() {
+    const style = window.getComputedStyle(root);
+    const currentBackgroundColor = style.backgroundColor;
+    if (currentBackgroundColor !== lastBackgroundColor) {
+        lastBackgroundColor = currentBackgroundColor; // Update the last known value
+        updateContainerBackground(lastBackgroundColor);
+    }
+}
+
+const observerCallback = (mutationsList, observer) => {
+    for(let mutation of mutationsList) {
+        if (mutation.type === 'attributes' && (mutation.attributeName === 'class' || mutation.attributeName === 'style')) {
+            checkForBackgroundColorChange();
+        }
+    }
+};
+
+const main = () => {
+    checkForBackgroundColorChange();
+
+    const observer = new MutationObserver(observerCallback);
+    observer.observe(root, { attributes: true, childList: false, subtree: false });
+}
+
+// main();
+document.addEventListener("DOMContentLoaded", main);
+""".strip()
+
+    opaque_container_css = """
+
+:root {{
+    --background-color: #ffffff; /* Default background color */
+}}
+
+
+div[data-testid="stVerticalBlockBorderWrapper"]:has(div.opaque-container-{id}):not(:has(div.not-opaque-container)) div[data-testid="stVerticalBlock"]:has(div.opaque-container-{id}):not(:has(div.not-opaque-container)) > div[data-testid="stVerticalBlockBorderWrapper"] {{
+    background-color: var(--background-color);
+    width: 100%;
+}}
+
+
+
+div[data-testid="stVerticalBlockBorderWrapper"]:has(div.opaque-container-{id}):not(:has(div.not-opaque-container)) div[data-testid="stVerticalBlock"]:has(div.opaque-container-{id}):not(:has(div.not-opaque-container)) > div[data-testid="element-container"] {{
+    display: none;
+}}
+
+
+div[data-testid="stVerticalBlockBorderWrapper"]:has(div.not-opaque-container):not(:has(div[class^='opaque-container-'])) {{
+    display: none;
+}}
+""".strip()
+
+    with fixed_container:
+        opaque_container = st.container()
+        non_opaque_container = st.container()
+        with opaque_container:
+            html(f"<script>{opaque_container_js}</script>", scrolling=False, height=0)
+            st.markdown(
+                f"<style>{opaque_container_css.format(id=key)}</style>",
+                unsafe_allow_html=True)
+            st.markdown(
+                f"<div class='opaque-container-{key}'></div>",
+                unsafe_allow_html=True,
+            )
+        with non_opaque_container:
+            st.markdown(
+                "<div class='not-opaque-container'></div>",
+                unsafe_allow_html=True,
+            )
+
+        return opaque_container.container(height=height, border=border)
 
 
 def scan_templates(folder_path):
